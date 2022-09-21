f013767f1fdb13919d93ed92bebefdd1f9499747
Diego Yon <diegoyon@outlook.com>
2022-09-21 13:49:21 -0600

    Add and fix linters

--- morse_code.rb
+++ morse_code.rb
@@ -1,55 +1,53 @@
 MORSE_DICT = {
-    'a' => '.-',
-    'b' => '-...',
-    'c' => '-.-.',
-    'd' => '-..',
-    'e' => '.',
-    'f' => '..-.',
-    'g' => '--.',
-    'h' => '....',
-    'i' => '..',
-    'j' => '.---',
-    'k' => '-.-',
-    'l' => '.-..',
-    'm' => '--',
-    'n' => '-.',
-    'o' => '---',
-    'p' => '.--.',
-    'q' => '--.-',
-    'r' => '.-.',
-    's' => '...',
-    't' => '-',
-    'u' => '..-',
-    'v' => '...-',
-    'w' => '.--',
-    'x' => '-..-',
-    'y' => '-.--',
-    'z' => '--..'
+  'a' => '.-',
+  'b' => '-...',
+  'c' => '-.-.',
+  'd' => '-..',
+  'e' => '.',
+  'f' => '..-.',
+  'g' => '--.',
+  'h' => '....',
+  'i' => '..',
+  'j' => '.---',
+  'k' => '-.-',
+  'l' => '.-..',
+  'm' => '--',
+  'n' => '-.',
+  'o' => '---',
+  'p' => '.--.',
+  'q' => '--.-',
+  'r' => '.-.',
+  's' => '...',
+  't' => '-',
+  'u' => '..-',
+  'v' => '...-',
+  'w' => '.--',
+  'x' => '-..-',
+  'y' => '-.--',
+  'z' => '--..'
 }
 
-
 def decode_char(char)
-    print MORSE_DICT.key(char).upcase
+  print MORSE_DICT.key(char).upcase
 end
 
 decode_char ".-"
 puts " "
 def decode_word(word)
-    words = word.split(' ')
-    words.each{ |word| 
-        decode_char(word)
-    } 
+  words = word.split(' ')
+  words.each { |word|
+    decode_char(word)
+  }
 end
 
-decode_word"-- -.--"
+decode_word "-- -.--"
 puts " "
 
 def decode(sentence)
   sentences = sentence.split('   ')
-  sentences.each{ 
-      |word| 
-      decode_word(word) 
-      print " "
+  sentences.each { |word|
+    decode_word(word)
+    print " "
   }
 end
 
