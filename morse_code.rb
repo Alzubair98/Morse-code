03354a7753e6293f3b0b181cdc78e31dc90b5ed7
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-09-21 22:40:22 +0300

    first 2 requirements

--- morse_code.rb
+++ morse_code.rb
@@ -1,1 +1,46 @@
+MORSE_DICT = {
+    'a' => '.-',
+    'b' => '-...',
+    'c' => '-.-.',
+    'd' => '-..',
+    'e' => '.',
+    'f' => '..-.',
+    'g' => '--.',
+    'h' => '....',
+    'i' => '..',
+    'j' => '.---',
+    'k' => '-.-',
+    'l' => '.-..',
+    'm' => '--',
+    'n' => '-.',
+    'o' => '---',
+    'p' => '.--.',
+    'q' => '--.-',
+    'r' => '.-.',
+    's' => '...',
+    't' => '-',
+    'u' => '..-',
+    'v' => '...-',
+    'w' => '.--',
+    'x' => '-..-',
+    'y' => '-.--',
+    'z' => '--..'
+}
+
+
+def decode_char(char)
+    print MORSE_DICT.key(char).upcase
+end
+
+decode_char ".-"
+puts " "
+def decode_word(word)
+    words = word.split(' ')
+    words.each{ |word| 
+        decode_char(word)
+    } 
+end
+
+decode_word"-- -.--"
+puts " "
 
